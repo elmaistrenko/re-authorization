76bc191383c9665afe784f70683cfceb4ef97b57
Yevgeniy Maistrenko <elmaistrenko@maximum-avto.ru>
2020-01-16 16:02:07 +0300

    ...

--- example/client.js
+++ example/client.js
@@ -1,10 +1,13 @@
-const createClient = require('../lib/client');
+const {ApiClient, MemoryStore} = require('../lib/client');
 
-const {request, login, logout, me, ws} = createClient({
+const client = new ApiClient({
     base: 'http://localhost:3456',
-    initialState: {
-        refresh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFscyI6eyJ1c2VybmFtZSI6ImEiLCJwYXNzd29yZCI6ImEiLCJyZW1lbWJlciI6dHJ1ZX0sInNlc3Npb25JZCI6IjVlMWUxYzU1NDMxZTdhMDg4OGE0OWY4NiIsInJlbWVtYmVyIjp0cnVlLCJpYXQiOjE1NzkwMzE2MzcsImV4cCI6MTg5NDM5MTYzN30.Lr_75fYhSM53HCk120lnKe9O9b0BCPoCyZSuvrq0rpU'
-    }
-});
+    store: new MemoryStore({
+        "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFscyI6eyJ1c2VybmFtZSI6ImEiLCJwYXNzd29yZCI6ImEiLCJyZW1lbWJlciI6dHJ1ZX0sInNlc3Npb25JZCI6IjVlMjA1OTFkMjQ1NWMwNGM0YTlhN2IwMiIsInJlbWVtYmVyIjp0cnVlLCJpYXQiOjE1NzkxNzgyNjksImV4cCI6MTg5NDUzODI2OX0.YhqZ7GXKRW2yyHhOmA7FRBQy34S5_o99evlP0RBmuyE",
+        "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImEiLCJzZXNzaW9uSWQiOiI1ZTIwNTkxZDI0NTVjMDRjNGE5YTdiMDIiLCJpYXQiOjE1NzkxNzgyNjksImV4cCI6MTU3OTE3OTE2OX0.yC5M7sNgWBCXJmrclVcfwQGAVdPa0M9PZANvUBD8QXQ"
+    })
+})
 
-ws('/cats').then(console.log, console.log);
+client.authorized().then(console.log)
+client.request('/cats').then(console.log, console.log);
+
